0c05a2d1a98ca55520955431f1c36880cb47ae41
louziyang <ziyangloua@gmail.com>
2025-08-15 12:48:29 +0800

    Update run-task.js

--- src/run-task.js
+++ src/run-task.js
@@ -8,7 +8,7 @@ const GITHUB_API_URL = 'https://api.github.com';
 const REPO_OWNER = process.env.GITHUB_REPOSITORY.split('/')[0]; // 从GITHUB_REPOSITORY环境变量中获取所有者
 const REPO_NAME = process.env.GITHUB_REPOSITORY.split('/')[1];  // 从GITHUB_REPOSITORY环境变量中获取仓库名称
 const REPO_PAT = process.env.REPO_PAT; // 从GitHub Secrets中获取的PAT
-const LAST_RUN_VARIABLE_NAME = 'LAST_TASK_RUN_TIMESTAMP'; // GitHub Repository Variable 的名称
+const LAST_RUN_VARIABLE_NAME = 'LAST_RUN_DATE'; // GitHub Repository Variable 的名称
 
 // 获取当前运行的 Node.js 解释器的路径
 const nodeExecutable = process.execPath; 
